4de3dc521bb3c32464afc59ee021f7b2c9db6303
KatyaKlema <katyaklema@gmail.com>
2020-11-21 00:05:47 +0300

    homework-6

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,55 +2,32 @@ package com.example.notes
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
-interface CellClickListener {
-    fun onCellClickListener(articleText: String, activity: AppCompatActivity)
-}
-
-class MainActivity : AppCompatActivity(), CellClickListener {
+class MainActivity : AppCompatActivity(),  Communicator {
+    private var mIsDualPane = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val articles = ArrayList<Article>()
-        articles.add(
-            Article(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "09.12.2018",
-                R.drawable.dreem,
-                0
-            )
-        )
-        articles.add(
-            Article(
-                "Implement this interface in activity or fragment wherever you wish to pass control. In this example this interface is implemented in MainActivity. Pass the reference of activity or fragment that implements CellClickListener to adapter like the line highlighted below is passing reference of activity in Adapter.",
-                "24.07.2010",
-                R.drawable.dreem,
-                1
-            )
-        )
-        articles.add(
-            Article(
-                "Recycler view uses several components to display data in a list format. The parent component is RecyclerView and it is implemented in activity or fragment. A recycler view is filled by views provided by LayoutManager (LinearLayoutManager or GridLayoutManager). A view is represented by view holder objects, data for each cell is set in view holder. We can access a view holder object in Adapter class. …",
-                "12.11.2011",
-                R.drawable.dreem,
-                2
-            )
-        )
-
-        val rv = findViewById<RecyclerView>(R.id.rv)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        val adapter = MyAdapter(articles, this)
-        rv.adapter = adapter
+        // If FragmentB is present in activity_main.xml, then we are in Tablet
+        // Else the app is running in handset
+        val fragmentBView = findViewById<View>(R.id.article_fragment)
+        mIsDualPane = fragmentBView?.visibility == View.VISIBLE
     }
 
-    override fun onCellClickListener(articleText: String, activity: AppCompatActivity) {
-        val intent = Intent(this, activity::class.java)
-        val b = Bundle()
-        b.putString("key", articleText)
-        intent.putExtras(b)
-        startActivity(intent)
+    override fun displayDetails(text: String, photoId: Int) {
+        if(mIsDualPane){
+            // If we are in Tablet
+            val articleFragment = supportFragmentManager.findFragmentById(R.id.article_fragment) as ArticleFragment?
+            articleFragment?.displayDetails(text, photoId)
+        }
+        else{
+            // When we are in Smart phone
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("text", text)
+            intent.putExtra("photoId", photoId.toString())
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/notes/RecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.notes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class RecyclerAdapter(private val context: Context?, private var list: List<Article>) :
+    RecyclerView.Adapter<RecyclerAdapter.ArticleViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val view = inflater.inflate(R.layout.list_item, parent, false)
+        return ArticleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val current = list[position]
+        holder.setData(current, position)
+        holder.setListeners()
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private var pos: Int = 0
+        lateinit var current: Article
+
+        fun setData(current: Article, position: Int) {
+            itemView.tvText.text = current.title
+            itemView.img_row.setImageResource(current.photoId)
+            itemView.date.text = current.date
+            this.pos = position
+            this.current = current
+        }
+
+        fun setListeners() {
+
+            itemView.setOnClickListener {
+                val myCommunicator = context as Communicator
+                myCommunicator.displayDetails(current.text, current.photoId)
+            }
+        }
+    }
+}
