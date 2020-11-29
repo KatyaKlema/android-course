97503fea8ab824761777aa6e684fff20eac54aaa
Ivan Shafran <shafran.fun@gmail.com>
2020-11-29 22:51:11 +0300

    refactor to use fragments properly

--- app/src/main/java/com/example/notes/ArticleFragment.kt
+++ app/src/main/java/com/example/notes/ArticleFragment.kt
@@ -8,14 +8,35 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+    companion object {
+        private const val ID_KEY = "id"
+        const val TAG = "ArticleFragment"
+
+        fun newInstance(articleId: Int): Fragment {
+            val fragment = ArticleFragment()
+
+            val arguments = Bundle()
+            arguments.putInt(ID_KEY, articleId)
+            fragment.arguments = arguments
+
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_article, container, false)
     }
 
-    fun displayDetails(articleId : Int) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val arguments = checkNotNull(arguments) { "Arguments are required" }
+        val id = arguments.getInt(ID_KEY)
+        displayDetails(id)
+    }
+
+    private fun displayDetails(articleId : Int) {
         txvText.text = articles[articleId].text
         imgArticle.setImageResource(articles[articleId].photoId)
     }
-
 }
